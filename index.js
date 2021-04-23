e9aa198ce430a48a41a7062900df5a9acb3a90e8
kavi2112 <80959281+kavi2112@users.noreply.github.com>
2021-04-23 18:01:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+
+//      {/* <input type="radio" value="Male" name="gender" /> Male
+//         <input type="radio" value="Female" name="gender" /> Female
+//         <input type="radio" value="Other" name="gender" /> Other */}
+//         <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+  
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
